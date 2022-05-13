d71d1bd6468e73d77e68bb233ba0c60d1351b337
George W <kinggeorgiehwanjohi@gmail.com>
2022-05-13 13:33:48 +0300

    added a bean for ApplicationProperties

--- simple-user-auth-service/src/main/java/com/master/demo/Application.java
+++ simple-user-auth-service/src/main/java/com/master/demo/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.master.demo.security.ApplicationProperties;
 import com.master.demo.utils.Utilities;
 
 @SpringBootApplication
@@ -13,15 +14,23 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@Bean
 	public Utilities util() {
 		return new Utilities();
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
+	@Bean
+	public SpringApplicationContext springApplicationContext() {
+		return new SpringApplicationContext();
+	}
+	@Bean(name="ApplicationProperties")
+	public ApplicationProperties applicationProperties() {
+		return new ApplicationProperties();
+	}
 }
